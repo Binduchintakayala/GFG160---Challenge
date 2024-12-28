87d1d4467ff4a0801064323b660cb89dc724de36
Hima Bindu Chintakayala <143824567+Binduchintakayala@users.noreply.github.com>
2024-12-28 23:33:10 +0530

    Day-17

---
+++ Non Repeating Character.py
@@ -0,0 +1,13 @@
+def nonRepeatingChar(s):
+    n = len(s)
+    for i in range(n):
+        found = False
+        for j in range(n):
+            if i != j and s[i] == s[j]:
+                found = True
+                break
+        if not found:
+            return s[i]
+    return '$'
+s = "racecar"
+print(nonRepeatingChar(s))
